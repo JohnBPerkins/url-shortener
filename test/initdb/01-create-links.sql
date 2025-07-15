93b2793fbe145ded20d08aae8c0ce1c5d2208942
John Perkins <john.mc.perkins@gmail.com>
2025-07-14 21:43:35 -0700

    fix: Tracks expiration time of links

--- test/initdb/01-create-links.sql
+++ test/initdb/01-create-links.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS public.links (
   code       TEXT PRIMARY KEY,
   url        TEXT NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  expires TIMESTAMPTZ NOT NULL DEFAULT now() + INTERVAL '24 hours'
 );
